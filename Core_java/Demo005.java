1a4032d3afa2fc3e0f833fb6078ea51d24cc1ed9
AJAYNAND <151133933+AJAYNAND@users.noreply.github.com>
2025-01-23 19:19:18 +0530

    While Loop

--- Core_java/Demo005.java
+++ Core_java/Demo005.java
@@ -1,6 +1,6 @@
 package Core_java;
 
-public class Demo005 {
+public class Demo005{
     public static void main(String[] args) {
         String Username = "user001";
         String Password = "user001";
