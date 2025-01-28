fde1712a82454e7fb9f3107a56546e65e0982ddb
AJAYNAND <151133933+AJAYNAND@users.noreply.github.com>
2025-01-28 22:33:34 +0530

    Recursive Fibonacci

--- Core_java/Demo028.java
+++ Core_java/Demo028.java
@@ -2,7 +2,7 @@ package Core_java;
 
 import java.util.Scanner;
 
-public class Demo028 {
+public class Demo028{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
