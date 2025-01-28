ee24ad1f412186567905532f8f783b3d7511240f
AJAYNAND <151133933+AJAYNAND@users.noreply.github.com>
2025-01-28 22:28:23 +0530

    Number Palindrome

--- Core_java/Demo023.java
+++ Core_java/Demo023.java
@@ -1,6 +1,6 @@
 package Core_java;
 
-public class Demo023 {
+public class Demo023{
     public static void main(String[] args) {
         System.out.println(isPalindrome(232));
         System.out.println(isPalindrome(345));
