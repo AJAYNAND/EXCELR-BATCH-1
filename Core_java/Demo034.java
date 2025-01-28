d4615009f89bfd2ae03b046c7a03bbe742eae13d
AJAYNAND <151133933+AJAYNAND@users.noreply.github.com>
2025-01-28 22:39:35 +0530

    Even or Odd

--- Core_java/Demo034.java
+++ Core_java/Demo034.java
@@ -1,5 +1,16 @@
 package Core_java;
 
 public class Demo034 {
+  public static void main(String[] args) {
+        int x=9;
+        if(x%2==0){
+            System.out.println("x is even");
+        } if(x==0){
+            System.out.println("x is zero");
+        }
+        else{
+            System.out.println("x is odd");
+        }
+    }
 
 }
